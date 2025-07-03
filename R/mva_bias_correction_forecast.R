fd32c2d26cf0ac1378a037d9a45a19f8054fe6dd
larscaspersen <lars.caspersen@uni-bonn.de>
2025-07-03 15:16:01 +0200

    handles now multiple columns

--- R/mva_bias_correction_forecast.R
+++ R/mva_bias_correction_forecast.R
@@ -26,8 +26,19 @@
 #' @param predicted data.frame containing the forecast data. Usually the same format as when running the
 #' extract_seaonal_forecast() function. Should contain columns: Year, Month, Day, Temp, unit
 #' 
+#' @param obs_cols character, indicates in which columns the observed weather variable is stored. Can be also a vector of column names, needs
+#' to match the length and order of the columns indicated in the pred_cols argument. Columns of predicted and observed are matched in the same order as 
+#' indicated in the vectors
+#' 
+#' @param pred_cols character, indicates in which columns the forecast weather variable is stored. Can be also a vector of column names, needs
+#' to match the length and order of the columns indicated in the obs_cols argument. Columns of predicted and observed are matched in the same order as 
+#' indicated in the vectors
+#' 
 #' @param unit_observed, character, by default "C". Indicates the unit of the input temperature. Bias correction is done in Kelvin.
 #' To indicate Fahrenheit, use "F". To indicate Kelvin use "K"
+#' 
+#' @param unit_predicted, character, by default "K". Indicates the unit of the forecast temperature. Bias correction is done in Kelvin.
+#' To indicate Fahrenheit, use "F". To indicate Celsius use "C"
 #' 
 #' @return data.frame, same format as "predicted", with additional column indicating correction factor and corrected (sub) daily temperature
 #' 
@@ -87,79 +98,118 @@
 #' @importFrom lubridate ym
 #' @importFrom stats aggregate
 #' @importFrom stats na.pass
+#' @importFrom assertthat assert_that
 #'  
 #' @export mva_bias_correction_forecast
 #' 
+#' 
 
 mva_bias_correction_forecast <- function(observed, predicted,
-                                         unit_observed = 'C'){
+                                         obs_cols = 'Tmean', pred_cols = 'temp',
+                                         unit_observed = 'C', unit_predicted = 'K'){
   
   #summarize both on a monthly basis
-  if(all(c('Month', 'Year') %in% colnames(observed)) == FALSE){
-    stop('Columns "Month" and "Year" need to be present in object "observed"')
+  if(all(c('Month', 'Year', obs_cols) %in% colnames(observed)) == FALSE){
+    stop(paste0('Columns ', paste(c('Month', 'Year', obs_cols), collapse = ', '), ' need to be present in object "observed"'))
   }
   
-  
-  fcst_sum <- stats::aggregate(predicted$temp, by = list(Month = predicted$Month, 
-                                                  Year = predicted$Year,
-                                                  model = predicted$model),FUN = mean,
-                        na.action = na.pass, na.rm = TRUE)
-  fcst_sum$mean_fcst <- round(fcst_sum$x, digits = 4)
-  fcst_sum$year_mo <- lubridate::ym(paste(fcst_sum$Year, fcst_sum$Month))
-  
-  
-  #select target column
-  if('Temp' %in% colnames(observed)){
-    observed$target_col = observed$Temp
-  } else if('Tmean' %in% colnames(observed)){
-    observed$target_col = observed$Tmean
-  } else if(all(c("Tmin", "Tmax") %in% colnames(observed))){
-    observed$target_col = (observed$Tmin + observed$Tmax)/2
-  } else {
-    stop('Temperature column with the name "Tmean", "Temp", "Tmin" together with "Tmax" need to be present in object "observed".')
+  #summarize both on a monthly basis
+  if(all(c('Month', 'Year', pred_cols) %in% colnames(predicted)) == FALSE){
+    stop(paste0('Columns ', paste(c('Month', 'Year', pred_cols), collapse = ', '), ' need to be present in object "predicted"'))
   }
+  
+  if(length(obs_cols) != length(pred_cols)){
+    stop('Targeted columns in predicted and observed need to be of same length')
+  }
+  
+  assertthat::assert_that(unit_observed %in% c('C', 'K', 'F'),
+                          unit_predicted %in% c('C', 'K','F'))
+  
+  res_df <- data.frame()
+  
   
   observed$year_mo <- lubridate::ym(paste(observed$Year, observed$Month))
-  #filter only relevant years and months
-  observed_sub <- observed[observed$year_mo %in% fcst_sum$year_mo,]
+  #filter only relevant years and monthssss
+  #if it coveres less than ione month, then take 15 years centered at year
   
-  observed_sum <-  stats::aggregate(observed_sub$target_col, by = list(Month = observed_sub$Month, 
-                                                               Year = observed_sub$Year),FUN = mean,
-                            na.action = stats::na.pass, na.rm = TRUE)
-  observed_sum$mean_obs <- round(observed_sum$x, digits = 4)
-  
-  
-  if(unit_observed == 'C'){
-    observed_sum$mean_obs <- observed_sum$mean_obs + 273.15
-  } else if(unit_observed == 'F'){
-    observed_sum$mean_obs <- (observed_sum$mean_obs + 459.67) * 5/9 
+  if(( max(predicted$Year)- min(predicted$Year))>15){
+    years_add <- (15 - (max(predicted$Year)- min(predicted$Year))) / 2
+    observed_sub <- observed[observed$Year >= min(predicted$Year) - years_add &
+                   observed$Year <= max(predicted$Year) + years_add &
+                   observed$Month %in% unique(predicted$Month), ]
   }
   
-  #----------------#
-  #correct monthly bias
-  #----------------#
-  #calculate mean temperature per month
-  clim.obs = mean(observed_sum$mean_obs, na.rm = T)
-  clim.fcst = mean(fcst_sum$mean_fcst, na.rm = T)
   
-  #calculate sd per month
-  sigma.e = sd(fcst_sum$mean_fcst, na.rm = T)
-  sigma.ref = sd(observed_sum$mean_obs, na.rm = T)
+  res_df <- predicted
   
-  #corrected monthly temperature of forecast data
-  fcst_call = ((fcst_sum$mean_fcst - clim.fcst) * (sigma.ref/sigma.e)) + clim.obs
+  #iterate over variables
+  for(i in 1:length(obs_cols)){
+    
+    #monthly mean of forecast
+    fcst_sum <- stats::aggregate(predicted[,pred_cols[i]], by = list(Month = predicted$Month, 
+                                                           Year = predicted$Year,
+                                                           model = predicted$model),FUN = mean,
+                                 na.action = na.pass, na.rm = TRUE)
+    fcst_sum$mean_fcst <- round(fcst_sum$x, digits = 4)
+    fcst_sum$year_mo <- lubridate::ym(paste(fcst_sum$Year, fcst_sum$Month))
+    
+    observed_sum <-  stats::aggregate(observed_sub[,obs_cols[i]], by = list(Month = observed_sub$Month, 
+                                                               Year = observed_sub$Year),FUN = mean,
+                            na.action = stats::na.pass, na.rm = TRUE)
+    observed_sum$mean_obs <- round(observed_sum$x, digits = 4)
+    
+    if(unit_observed == 'C'){
+      observed_sum$mean_obs <- observed_sum$mean_obs + 273.15
+      } else if(unit_observed == 'F'){
+        observed_sum$mean_obs <- (observed_sum$mean_obs + 459.67) * 5/9 
+      }
+    
+    if(unit_predicted == 'C'){
+      fcst_sum$mean_fcst <- fcst_sum$mean_fcst + 273.15
+    } else if(unit_predicted == 'F'){
+      fcst_sum$mean_fcst <- (fcst_sum$mean_fcst + 459.67) * 5/9 
+    }
+    
+    
+    #----------------#
+    #correct monthly bias
+    #----------------#
+    #calculate mean temperature per month
+    clim.obs = mean(observed_sum$mean_obs, na.rm = T)
+    clim.fcst = mean(fcst_sum$mean_fcst, na.rm = T)
+    
+    #calculate sd per month
+    sigma.e = sd(fcst_sum$mean_fcst, na.rm = T)
+    sigma.ref = sd(observed_sum$mean_obs, na.rm = T)
+    if(is.na(sigma.ref)) sigma.ref <- sigma.e
+    
+    #corrected monthly temperature of forecast data
+    fcst_call = ((fcst_sum$mean_fcst - clim.fcst) * (sigma.ref/sigma.e)) + clim.obs
+    
+    #add info to forecast summary
+    fcst_sum$fcst_call = fcst_call
+    
+    #calc correction factor that is used to scale the daily observation of forecast
+    fcst_sum[,paste0(pred_cols[i], '_corfac')] <- (fcst_sum$fcst_call / fcst_sum$mean_fcst)
+    
+    
+    #select appropriate columns
+    fcst_sum <- fcst_sum[,c('Year', 'Month', 'model', paste0(pred_cols[i], '_corfac'))]
+    
+    res_df <-  merge(res_df, fcst_sum, by = c('Year', 'Month', 'model')) 
+    res_df[,paste0(pred_cols[i], '_corrected')] = res_df[, pred_cols[i]] * res_df[,paste0(pred_cols[i], '_corfac')] 
+    
+    #bring back to original format
+    if(unit_predicted == 'C'){
+      res_df[,paste0(pred_cols[i], '_corrected')] <- res_df[,paste0(pred_cols[i], '_corrected')] - 273.15
+    } else if(unit_predicted == 'F'){
+      res_df[,paste0(pred_cols[i], '_corrected')]  <- (res_df[,paste0(pred_cols[i], '_corrected')] *  (9/5)) - 459.67 
+    }
+  }
   
-  #add info to forecast summary
-  fcst_sum$fcst_call = fcst_call
-  
-  #calc correction factor that is used to scale the daily observation of forecast
-  fcst_sum$corr_fact <- (fcst_sum$fcst_call / fcst_sum$mean_fcst)
-  
-  
-  #select appropriate columns
-  fcst_sum <- fcst_sum[,c('Year', 'Month', 'model', 'mean_fcst', 'fcst_call', 'corr_fact')]
-  
-  combined <-  merge(predicted, fcst_sum, by = c('Year', 'Month', 'model')) 
-  combined$temp_corrected = combined$temp * combined$corr_fact 
-  return(combined)
+  #drop the correction factors
+  col_drop <- which(grepl(pattern = '*_corfac', colnames(res_df)))
+
+  return(res_df[,-col_drop])
 }
+
